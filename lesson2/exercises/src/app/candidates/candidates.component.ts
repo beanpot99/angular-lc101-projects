f589094576a4ca660ad0a9e414d6216fc443298e
Grace Thompson <Grace.thompson17@aol.com>
2022-03-21 15:23:56 -0500

    all changes made except input box won't go away

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidatesComponent implements OnInit {
    missionName = "LaunchCode Moonshot"
-    editMissionName = 'false'
+    editMissionName = false
    candidates = [
     {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
     {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
@@ -27,10 +27,13 @@ export class CandidatesComponent implements OnInit {
     if(!this.crew.includes(obj)){
       this.crew.push(obj)
     }
+    
   }
 
-
-  // BONUS: Code the changeMissionName function here:
+  changeMissionName(name:string){
+      this.missionName=name;
+      return false;
+  }
 
 
 }
